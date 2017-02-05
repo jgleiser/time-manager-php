f00717177c84bf7fdefd2574bb8fff579971e715
Jose Gleiser <jose.gleiser@gmail.com>
2017-02-05 00:13:29 -0300

    index: Added spl_autoload_register

--- public/index.php
+++ public/index.php
@@ -10,6 +10,10 @@ $app = new \Slim\App(array(
     "settings" => $config
 ));
 
+spl_autoload_register(function ($classname) {
+    require_once("../src/classes/" . $classname . ".php");
+});
+
 include_once '../src/container.php';
 include_once '../src/routes.php';
 
